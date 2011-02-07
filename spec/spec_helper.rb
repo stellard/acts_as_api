b249aa6b326c57bee8f229c6ccca55cf9cf8e912
stellard <scott.ellard@gmail.com>
2011-02-07 15:39:41 +0000

    cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,3 @@
-# begin
-#   #require 'spec'
-# rescue LoadError
-#   require 'rubygems' unless ENV['NO_RUBYGEMS']
-#   gem 'rspec'
-#   require 'spec'
-# end
-
-# $:.unshift(File.dirname(__FILE__) + '/../lib')
-# require 'rubygems'
-# require 'active_support'
-# require 'active_record'
-# require 'action_controller'
-# 
-# require 'acts_as_api'
-
 # Configure Rails Envinronment
 ENV["RAILS_ENV"] = "test"
 
